751acecb6f736f65180a3a482b3bb681a959c1ae
DESKTOP-D45T7L3\PC <dtungnguyen25@gmail.com>
2025-05-12 17:57:41 +0700

    Add Missile Turret, Coroutine Destroy bullet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class Bullet : MonoBehaviour
 {
@@ -7,8 +8,13 @@ public class Bullet : MonoBehaviour
     public float speed = 70f;
     public GameObject impactEffect;
     // Reference to the impact effect prefab to be instantiated on hit
+    public float delayDestroy = 1f;
+    // Delay before destroying the bullet after impact
+    private bool destroyed;
+    // Time to wait before destroying the bullet
 
-    public void Seek (Transform _target)
+
+    public void Seek(Transform _target)
     {
         target = _target;
         // Set the target to the specified transform    
@@ -16,7 +22,7 @@ public class Bullet : MonoBehaviour
 
     void Start()
     {
- 
+
     }
 
     // Update is called once per frame
@@ -24,35 +30,52 @@ public class Bullet : MonoBehaviour
     {
         if (target == null)
         {
-            Destroy(gameObject);
-            // If no target is set, destroy the bullet
-            return;
-        }        
-
-        Vector3 direction = target.position - transform.position;
-        // Calculate the direction to the target
-        float distanceThisFrame = speed * Time.deltaTime;
-        // Calculate the distance to move this frame
-        if (direction.magnitude <= distanceThisFrame)
-        // Check if the bullet is close enough to the target
-        {
-            HitTarget();
-            return;
+            if (!destroyed)
+                StartCoroutine(DestroyBullet());
+            // If the target is null, start the coroutine to destroy the bullet
         }
-        transform.Translate(direction.normalized * distanceThisFrame, Space.World);
-        // Move the bullet towards the target in the direction of the target
 
+        if (!destroyed)
+        {
+
+            Vector3 direction = target.position - transform.position;
+            // Calculate the direction to the target
+            transform.forward = direction;
+            // Rotate the bullet to face the target
+            float distanceThisFrame = speed * Time.deltaTime;
+            // Calculate the distance to move this frame
+            if (direction.magnitude <= distanceThisFrame)
+            // Check if the bullet is close enough to the target
+            {
+                HitTarget();
+                return;
+            }
+            transform.Translate(direction.normalized * distanceThisFrame, Space.World);
+            // Move the bullet towards the target in the direction of the target
+        }
+    }
+    IEnumerator DestroyBullet()
+    {
+        destroyed = true;
+        // If the target is null, set destroyed to true
+        yield return new WaitForSeconds(delayDestroy);
+        // Wait for the specified delay before destroying the bullet
+        Destroy(gameObject);
+        // Destroy the bullet game object
     }
 
     void HitTarget()
     {
         Debug.Log("Hit " + target.name);
-        // Log the name of the target hit
-        Instantiate(impactEffect, transform.position, transform.rotation);
-        // Instantiate the impact effect at the bullet's position and rotation
+        if (!destroyed)
+            StartCoroutine(DestroyBullet());
+        // If the bullet has not been destroyed, start the coroutine to destroy it
+        if (impactEffect)       
+        // Check if the impact effect prefab is assigned
+        {
+            // Instantiate the impact effect at the bullet's position and rotation
+            Instantiate(impactEffect, transform.position, transform.rotation);
+        }
 
-        Destroy(gameObject);
-        // Destroy the bullet when it hits the target
-        // You can also add code here to deal damage to the target or play a hit effect
     }
 }
